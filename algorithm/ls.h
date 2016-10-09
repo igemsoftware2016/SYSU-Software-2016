09063682a5d9353df2d4fb0a2f90fea0a3fa8b13
Lin Xiaoya <daiga@live.cn>
2016-10-09 20:22:10 +0800

    Revert "Adding promoter and RBS calculating module"
    This reverts commit dbb016bf981f390a66399f946ff76e179d652c63.

--- algorithm/ls.h
+++
@@ -1,7 +0,0 @@
-#ifndef __LS_IOST
-#define __LS_IOST
-#include "./eigen/include/eigen3/Eigen/Dense"
-#include "./eigen/include/eigen3/Eigen/LU"
-
-void leastSquares(float *x, int n, float *y, float *result);
-#endif
