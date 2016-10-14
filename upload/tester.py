c8bac714cc76ff951c12cf11f9c52a064f5ccdfc
eazy_jobb <kararyou@qq.com>
2016-10-14 19:51:01 +0800

    sheet tester

--- upload/tester.py
+++ upload/tester.py
@@ -4,13 +4,30 @@ book = open_workbook('state_1_template.xlsx')
 sheet = book.sheet_by_index(0)
 
 isProduct = (sheet.cell(0,1) == 'Products')
-numMater = sheet.cell(0,3)
+numMater = int(sheet.cell(0,3).value)
 
 for i in xrange(2, 2 + numMater):
-    mater_name = 
+    mater_name = sheet.cell(i, 0).value
+    init_con = sheet.cell(i, 1).value
+    lower = sheet.cell(i, 2).value
+    upper = sheet.cell(i, 3).value
+    max_con = sheet.cell(i, 4).value
+    print mater_name, init_con, lower, upper, max_con
 
-for i in xrange(sheet.nrows):
-    print 'ROW', i
-    for j in xrange(sheet.ncols):
-        print sheet.cell(i, j).value
+cnt = 2 + numMater + 1
+
+numEnv = int(sheet.cell(cnt, 1).value)
+
+for i in xrange(cnt + 1, cnt + 1 + numEnv):
+    envBtr = sheet.cell(i, 0).value
+    print envBtr
+
+cnt += 1 + numEnv + 1
+
+matNum = int(sheet.cell(cnt, 1).value)
+
+for i in xrange(cnt + 1, cnt + 1 + matNum):
+    mater_name = sheet.cell(i, 0).value
+    con = sheet.cell(i, 1).value
+    print mater_name, con
 
