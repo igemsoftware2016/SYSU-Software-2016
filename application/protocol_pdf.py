e39c4ec38978cc21c2150d98ae4d9cb0e1fbeb15
Lin Xiaoya <daiga@live.cn>
2016-10-19 14:01:57 +0800

    External protocal generator discarded.

--- application/protocol_pdf.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-if __name__ == "__main__":
-	if sys.argv[1]:
-		os.system("wkhtmltopdf ./tmp_protocol.html " + os.path.join(basedir, 'static/pdf/%s.pdf' % sys.argv[1]))
