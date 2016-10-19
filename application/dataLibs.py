f130633b6d714cbf38aff0806e255cd91b89ea32
Lin Xiaoya <daiga@live.cn>
2016-10-19 16:04:13 +0800

    Promoter RBS strength range fix

--- application/dataLibs.py
+++ application/dataLibs.py
@@ -511,8 +511,8 @@ def get_state_saved(state_id):
                         if tmppro:
                             all_pro.append(tmppro)
                     total_pro.extend(all_pro)
-                    ret_enzy["strength"]["promoter_lower"] = all_pro[0].strength
-                    ret_enzy["strength"]["promoter_upper"] = all_pro[len(all_pro) - 1].strength
+                    ret_enzy["strength"]["promoter_lower"] = all_pro.min()
+                    ret_enzy["strength"]["promoter_upper"] = all_pro.max()
                     ret_enzy["strength"]["promoter"] = []
                     for pro in all_pro:
                         ret_enzy["strength"]["promoter"].append({"s": pro.strength, "info": pro.id})
@@ -522,8 +522,8 @@ def get_state_saved(state_id):
                         if tmprbs:
                             all_rbs.append(tmprbs)
                     total_rbs.extend(all_rbs)
-                    ret_enzy["strength"]["RBS_lower"] = all_rbs[0].strength
-                    ret_enzy["strength"]["RBS_upper"] = all_rbs[len(all_rbs) - 1].strength
+                    ret_enzy["strength"]["RBS_lower"] = all_rbs.min()
+                    ret_enzy["strength"]["RBS_upper"] = all_rbs.max()
                     ret_enzy["strength"]["RBS"] = []
                     for rbss in all_rbs:
                         ret_enzy["strength"]["RBS"].append({"s": rbss.strength, "info": rbss.id})
